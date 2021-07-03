352fbafd01c61a7c9d8fed66f23a152d8c7f0543
RodrigoFaccio <rodrigo1612fm@gmail.com>
2021-07-03 13:25:12 -0300

    Add fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import * as Font from 'expo-font';
+import { useFonts } from 'expo-font';
 export default function App() {
+  const [loaded] = useFonts({
+    Geometria: require('./assets/fonts/Geometria/Geometria.ttf'),
+  });
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text> Open dasdasup App.tsx to start working on your app!</Text>
+      <Text style={{fontFamily:'Geometria'}}>Geometria</Text>
       <StatusBar style="auto" />
     </View>
   );
